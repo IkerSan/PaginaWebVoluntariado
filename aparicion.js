54a0e38c64d7b60c7b208fc92a3830893cda88c9
IkerSan <cosasquequieres@gmail.com>
2025-04-15 10:38:45 +0200

    archivos js añadidos

--- aparicion.js
+++
@@ -1,7 +0,0 @@
-window.addEventListener("scroll", () => {
-    document.querySelectorAll(".fade-in").forEach(el => {
-      if (el.getBoundingClientRect().top < window.innerHeight - 150) {
-        el.classList.add("visible");
-      }
-    });
-  });
